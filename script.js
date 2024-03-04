6752dfa197de804fb53b75888710eb4444d3a07e
Daniele Frisone <daniele.frisone@hotmail.it>
2024-03-04 22:15:12 +0100

    commenti al codice

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-let currentOperation = '';
-let currentNumber = '';
+let currentOperation = ''; //stringa perché usa la funzione eval che ci aveva insegnato scrimin
+let currentNumber = ''; //stringa perché usa la funzione eval che ci aveva insegnato scrimin
 let total = 0;
 
-function appendNumber(number) {
+function appendNumber(number) { //aggiunge un numero. se clicco 7 poi 9 poi 3 diventa 793
   currentNumber += number;
   console.log("appendNumber, currentNumber " + currentNumber)
   updateDisplay(currentNumber);
 }
 
 function appendOperator(operator) {
-  if (currentNumber !== '') {
+  if (currentNumber !== '') {  //aggiunge l'operatore + - * /
     console.log("currentNumber: " + currentNumber)
     currentOperation += currentNumber + operator;
     console.log("currentOperation: " + currentOperation)
     currentNumber = '';
-  } else if (currentOperation !== '') {
+  } else if (currentOperation !== '') { // se l'ultimo elemento è un operatore lo sostituisce tipo clicco + e poi / salva solo /
     currentOperation = currentOperation.slice(0, -1) + operator;
     console.log("currentOperation 2: " + currentOperation)
   }
@@ -33,7 +33,7 @@ function calculate() {
   if (currentNumber !== '') {
     currentOperation += currentNumber;
     console.log(currentOperation)
-    total = eval(currentOperation); /* la mitica funzione di scrimin. trasforma una stringa in calcolo numerico */
+    total = eval(currentOperation); /* la mitica funzione di scrimin: EVAL . trasforma una stringa in calcolo numerico */
     updateDisplay(total);
     currentOperation = '';
     currentNumber = '';
